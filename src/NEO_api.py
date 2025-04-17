9b2bd07c03d128b098ed9774500f0a4384b9af0b
Matthew Thomas <mjt3248@my.utexas.edu>
2025-04-17 05:06:21 +0000

    made delete route and one other

--- src/NEO_api.py
+++ src/NEO_api.py
@@ -33,31 +33,64 @@ rdb = redis.Redis(host=REDIS_IP, port=6379, db=3)
 @app.route('/data', methods = ['POST'])
 def fetch_neo_data():
     """
-
+    This function downloads the data as a csv and uploads it to Redis.
+        Args:
+            None
+        Returns:
+            message (str): message indicating whether data push was successful
     """
     try:
         data = pd.read_csv('/app/neo.csv')
-        dict_data = {}
         for idx, row in data.iterrows():
-            dict_data = json.dumps({'Object' : row['Object'],'CA DistanceNominal (au)' : row['CA DistanceNominal (au)'], 'CA DistanceMinimum (au)' : row['CA DistanceMinimum (au)'], 'V relative(km/s)' : row['V relative(km/s)'], 'V infinity(km/s)':  row['V infinity(km/s)'], 'H(mag)' : row['H(mag)'], 'Diameter' : row['Diameter'],'Rarity' : row['Rarity']}, sort_keys=True)
-            rd.set(row['Close-Approach (CA) Date'], dict_data)
-        if rd.scan():
+            dict_data = {'Object' : row['Object'],'CA DistanceNominal (au)' : row['CA DistanceNominal (au)'], 'CA DistanceMinimum (au)' : row['CA DistanceMinimum (au)'], 'V relative(km/s)' : row['V relative(km/s)'], 'V infinity(km/s)':  row['V infinity(km/s)'], 'H(mag)' : row['H(mag)'], 'Diameter' : row['Diameter'],'Rarity' : row['Rarity']}
+            rd.set(row['Close-Approach (CA) Date'], json.dumps(dict_data, sort_keys=True))
+        if len(rd.keys('*')) == len(data):
             return 'success loading data\n'
-
+        else:
+            return 'failed to load all data into redis'
     except Exception as e:
-        logging.error(f"Error fetching NEO data: {e}")
+        logging.error(f"Error downloading NEO data: {e}")
         return f"Error fetching data: {e}\n"
 
 @app.route('/data', methods = ['GET'])
 def return_neo_data():
     dat = {}
     for key in rd.keys('*'):
+        key = key.decode('utf-8')
+        try:
+            val = json.loads(rd.get(key).decode('utf-8'))
+            dat[key] = val
+        except:
+            logging.error(f'Error retrieving data at {key}')
         
-        dat[hash] = vals
+    return json.dumps(dat)
+
+@app.route('/data', methods = ["DELETE"])
+def delete_neo_data():
+    rd.flushdb()
+    if not rd.keys():
+        return 'Database flushed\n'
+    else:
+        return "Database failed to clear\n"
+
+@app.route('/data/<year>', methods = ['GET'])
+def get_data_by_year(year):
+    '''
+    This function returns the data for NEO's that will approach Earth in a given year.
+        Args:
+            year (str): the year for which you want NEO data for
+        Returns:
+            dat (dict) - subset of the data
+    '''
+    dat = {}
+    for key in rd.keys('*'):
+        key = key.decode('utf-8')
+        if key.split('-')[0] == year:
+            dat[key] = json.loads(rd.get(key).decode('utf-8'))
     return dat
 
+    
 
-    return rd.hgetall()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
