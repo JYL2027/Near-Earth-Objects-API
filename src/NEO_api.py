7db266957891175c402fb4478db6bd9f16e01e69
Jacky Lin <jylin2027@gmail.com>
2025-04-22 11:50:52 -0500

    adding job in main script

--- src/NEO_api.py
+++ src/NEO_api.py
@@ -170,9 +170,121 @@ def query_velocity():
     return dat
 
 
+@app.route('/jobs', methods=['POST'])
+def create_job() -> Response:
+    """
+    This function is a API route that creates a new job
 
+    Args:
+        None
+    
+    Returns:
+        The function returns a Flask json reponse of the created job or if it failed to create the job
+    """
 
+    logging.debug("Creating job...")
+    if not request.json:
+        return jsonify("Error, invalid input for job")
+    
+    # Data packet must be json
+    params = request.get_json()
+    
+    start_date = params.get("start_date")
+    end_date = params.get("end_date")
 
+    if start_date is None or end_date is None:
+        return jsonify("Error missing start_date or end_date parameters")
+
+    # Check if ID's are valid
+    keys = rd.keys()
+    ID = []
+    logging.info("Filtering out Dates... ")
+    for key in keys:
+        # Decode the Key
+        ID.append(key.decode('utf-8'))
+
+    if start_date not in ID or end_date not in ID:
+            return jsonify("Error: invalid HGNC ID's or no Data in Redis")
+    
+    # Add a job
+    job = add_job(start_date, end_date)
+
+    logging.debug(f"Job created and queued successfully.")
+    return jsonify(job)
+
+@app.route('/jobs', methods=['GET'])
+def list_jobs() -> Response:
+    """
+    This function is a API route that lists all the job IDs
+
+    Args:
+        None
+
+    Returns:
+        The function returns all of the existing job ID's as a Flask json response
+    """
+    logging.debug("Listing job ID's...")
+
+    job_ids = []
+    job_keys = jdb.keys()
+
+    if not job_keys:
+        logging.warning("No IDs found in Redis")
+        return jsonify("No job ID's currently")
+    
+    for key in job_keys:
+        job_ids.append(key.decode('utf-8'))
+    
+    logging.debug("All job ID's found successfully")
+    return jsonify(job_ids)
+
+@app.route('/jobs/<jobid>', methods=['GET'])
+def get_job(jobid: str) -> Response:
+    """
+    This function is a API route that retrieves job details by ID
+
+    Args:
+        jobid is the ID of the job you want to get information about as a string
+
+    Returns:
+        The function returns all of the job information for the given job ID as a Flask json response
+    """
+    logging.debug("Retrieving job details...")
+
+    job = get_job_by_id(jobid)
+
+    if not job:
+        return jsonify("Error job not found")
+    
+    return jsonify(job)
+
+@app.route('/results/<jobid>', methods = ['GET'])
+def check_result(jobid : str) -> Response:
+    """
+    This function given a jobid will retrieve the job results or to see if the job is still in progress.
+
+    Args:
+
+        jobid is the ID of the job you want to get information about as a string
+
+    Returns:
+        Returns the results of a job as a Flask response
+    """
+
+    job = get_job_by_id(jobid)
+    result = get_job_result(jobid)
+    logging.debug("Retrieved job and result")
+    
+    if not job:
+        return jsonify("Error job not found")
+    
+    if job["status"] != "complete":
+        return jsonify(f"Job {jobid} is still in progress.")
+
+    if not result:
+        return jsonify("No result found")
+
+    return Response(result)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
